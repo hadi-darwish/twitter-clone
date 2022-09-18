31108f918487a88a6656f2a2327befd6f07adc93
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-18 18:56:12 +0300

    restyling the profile page search bar

--- frontend/javascript/scripts.js
+++ frontend/javascript/scripts.js
@@ -2,24 +2,6 @@ const popup = document.getElementById("popup");
 const signup = document.getElementById("signup");
 const close = document.getElementById("closePop");
 const edit_profile = document.getElementById("edit_profile");
-// const prevbtn = document.querySelectorAll(".btn-prev");
-// const nextbtn = document.querySelectorAll(".btn-next");
-// const formstep = document.querySelectorAll(".form-step");
-
-
-// let formstepsnum = 0 ;
-
-// nextbtn.forEach( btn => {
-//     btn.addEventListener('click' , () => {
-//        formstep++;
-//        UpdateFormSteps();
-//     })
-// })
-
-// function UpdateFormSteps(){
-//     formstep[formstep].classList.add("form-step-acive")
-// }
-
 
 signup.addEventListener("click", openPopup)
 function openPopup(){
