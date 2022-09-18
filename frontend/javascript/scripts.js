38d85a0072a4078841a898d824458cd933c40e58
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-09-18 23:51:18 +0300

    commit

--- frontend/javascript/scripts.js
+++ frontend/javascript/scripts.js
@@ -1,25 +1,81 @@
 const popup = document.getElementById("popup");
 const signup = document.getElementById("signup");
 const close = document.getElementById("closePop");
+const next = document.getElementById("next-btn");
 
+//hadi
+const login = document.getElementById("login");
+const email = document.getElementById("email-login");
+const password = document.getElementById("password-login");
+const errorMsg = document.getElementById("error-messages");
+const form = document.querySelector("form");
+let error = "";
 
-signup.addEventListener("click", openPopup)
-function openPopup(){
-    popup.classList.add("open-popup");
+login.onclick = (event) => {
+  event.preventDefault();
+  errorMsg.style.display = "none";
+  error = "";
+  if (!(checkEmail() && checkPassword())) {
+    console.log(!(checkEmail() && checkPassword()));
+    errorMsg.style.display = "block";
+    errorMsg.innerHTML = error;
+    return false;
+  } else {
+    let shi = {
+      method: "POST",
+      body: new URLSearchParams({
+        email: email.value,
+        password: password.value,
+      }),
+    };
+    fetch("http://localhost/twitter-apis/login-confirm.php", shi)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.confirmation == "not found") {
+          error += "Email Not Found<br>";
+          errorMsg.style.display = "block";
+          errorMsg.innerHTML = error;
+        } else if (data.confirmation == "false") {
+          error += "WRONG PASSWORD!!<br>";
+          errorMsg.style.display = "block";
+          errorMsg.innerHTML = error;
+        } else {
+          localStorage.setItem("user_id", data.id);
+          location.replace("../feed.html");
+        }
+      });
+  }
+};
+function checkEmail() {
+  if (!/(^[\w-\.]{3,})+@(([\w-]{5,})+\.)+[\w-]{2,4}$/.test(email.value)) {
+    email.style.borderColor = "red";
+    error +=
+      "Please enter email with at least 3 characters before @ and 5 after!!<br>";
+    return false;
+  }
+  email.style.borderColor = "green";
+  return true;
 }
 
+function checkPassword() {
+  if (!/^(\w){12,}$/.test(password.value)) {
+    password.style.borderColor = "red";
+    error += "password is at least 12 characters!!<br>";
+    return false;
+  }
+
+  password.style.borderColor = "green";
+  return true;
+}
+//hadi
+
+signup.addEventListener("click", openPopup);
+function openPopup() {
+  popup.classList.add("open-popup");
+}
 
 window.onload = signup;
-
-close.addEventListener("click", closePopup)
-function closePopup(){
-    popup.classList.remove("open-popup");
+close.addEventListener("click", closePopup);
+function closePopup() {
+  popup.classList.remove("open-popup");
 }
-
-
-
-
-
-
-
-
